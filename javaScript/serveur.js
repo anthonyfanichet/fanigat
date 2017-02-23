4855e87ab8f7568251c0839c5d0c7c7ea60a25db
Anthony fanichet <Anthony fanichet>
2017-02-23 18:13:30 +0100

    adding serveur.js

---
+++ javaScript/serveur.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var fs = require("fs");
+var url = require("url");
+var path = require("path");
+
+http.createServer(function(request, response) {
+	
+	var uri = url.parse(request.url).pathname;
+	var filname = path.join(__dirname, uri);
+	console.log("before all file:" + filname);
+	console.log("before all  dir:" + __dirname);
+
+	if(request.url === "/Home"){
+		sendFileContent(response, "../pages/home.html", "text/html");
+	}
+	else if(request.url === "/About"){
+		sendFileContent(response, "../pages/about.html", "text/html");
+	}
+	else if(request.url === "/Contact"){
+		sendFileContent(response, "../pages/contact.html", "text/html");
+	}
+	else if(request.url === "/Catalogue"){
+		sendFileContent(response, "../pages/portfolio.html", "text/html");
+	}
+	else if(path.extname(filname) == ".js"){
+		sendFileContent(response, "../" + request.url.toString().substring(1), "text/javascript");
+	}
+	else if(path.extname(filname) == ".css"){
+		console.log("IN CSS :" + filname);
+		console.log("request.url : " + request.url.toString().substring(1));
+		sendFileContent(response,"../"+ request.url.toString().substring(1), "text/css");
+	}
+	else{
+		console.log("Requested URL is: " + request.url);
+		response.end();
+	}
+}).listen(3000);
+
+function sendFileContent(response, fileName, contentType){
+	fs.readFile(fileName, function(err, data){
+		if(err){
+			response.writeHead(404);
+			response.write("Not Found!");
+		}
+		else{
+			response.writeHead(200, {'Content-Type':contentType});
+			response.write(data);
+		}
+		response.end();
+	});
+}
+
